ea01ca6ea7b25ef3d306be90058caabd607610be
arcyro <arek@arcyro.pl>
2017-08-14 18:16:36 +0200

    add i18n #4439

--- src/main/java/pl/coderslab/config/WebConfiguration.java
+++ src/main/java/pl/coderslab/config/WebConfiguration.java
@@ -5,7 +5,11 @@ import org.h2.server.web.WebServlet;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.dialect.springdata.SpringDataDialect;
+
+import java.util.Locale;
 
 @Configuration
 public class WebConfiguration {
@@ -14,6 +18,13 @@ public class WebConfiguration {
     @Bean
     public SpringDataDialect springDataDialect() {
         return new SpringDataDialect();
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("pl"));
+        return slr;
     }
 
 //    @Bean
